107face0f7889396004123ada218cc189548a03c
yuuya-1205 <trip.throw.t3@gmail.com>
2020-06-03 07:43:08 +0900

    add login.blade.php

---
+++ login.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+
+
+<title>login</title>
+
+
+<a href="http://127.0.0.1:8000/login">login</a>
+<a href="http://127.0.0.1:8000/register">register</a>
+
+</header>
+
+<style>
+    .title {
+      float: left;
+      font-size: 20px;
+    }
+
+    a {
+      float: right;
+      padding: 10px;
+    }
+
+    
+</style>
+</head>
+<body>
+<form action="#" method="post">
+  <p>Login</p>
+    <p>E-mail Adress：<input type="text" name="adress"></p>
+    <p>Password:<input type="text" name="password"></p>
+  <p><input type="submit" value="Login"></p>
+</form>
+</body>
+</html>
